1bdf2f38e18eedeccd8aa1e3b8c87c4bddde19e6
sudhirmandarapu <sudhirmandarapu@gmail.com>
2018-09-07 14:57:06 +1000

    feat: read LIWC features and add them to the feature array

--- preprocessor.py
+++ preprocessor.py
@@ -5,6 +5,10 @@ import os
 
 class Preprocessor:
     def __init__(self, path):
+        """
+        Pre-processes the transcripts into to an array of features that can be inputted to an ML model.
+        :param path: Location of all the transcripts.
+        """
         self.path = path
 
     def get_all_transcript_features(self):
@@ -15,13 +19,14 @@ class Preprocessor:
         x = []
         y = []
         expected_dict = self._build_expected_dict()
+        liwc_features = self._build_liwc_feature_dict()
         for file in os.listdir(self.path):
             name, ext = os.path.splitext(file)
             if ext != '.csv' or name[4:] != 'TRANSCRIPT':
                 continue
-            x.append(self._get_features_for_transcript(file))
+            x.append(self._get_features_for_transcript(file, liwc_features))
             y.append(int(expected_dict[name[:3]]))
-        return x, y
+        return np.array(x, dtype='float32'), np.array(y, dtype='int32')
 
     def compile_all_transcripts(self, dest_dir):
         for file in os.listdir(self.path):
@@ -46,19 +51,29 @@ class Preprocessor:
                     expected_dict[row[0]] = row[1] == '1'
         return expected_dict
 
-    def _get_features_for_transcript(self, file):
+    def _get_features_for_transcript(self, file, liwc_features):
         """
         Compiles the features array for a particular transcript.
         :param file: Name of the transcript.
         :return: Features array.
         """
-        features = []
+        name, _ = os.path.splitext(file)
+        features = liwc_features[name]
         avg_res = self._get_average_response_length(file)
         features.append(avg_res)
         return features
 
-    def _get_liwc_features(self):
-        return
+    def _build_liwc_feature_dict(self):
+        liwc_dict = {}
+        with open(self.path+'/liwc_features.csv') as csv_file:
+            reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
+            for row in reader:
+                row = row[0].split(',')
+                if row[0] == 'Filename':
+                    continue
+                name, _ = os.path.splitext(row[0])
+                liwc_dict[name] = row[3:]
+        return liwc_dict
 
     def _get_average_response_length(self, file):
         """
