bc5c7210ea1007fd5878800b83adadd7ab644bac
sudhirmandarapu <sudhirmandarapu@gmail.com>
2018-10-23 09:41:53 +1100

    chore: upsample training set and print metrics on final test set

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from preprocessor import Preprocessor
 import transcript
 import numpy as np
 from models.log_reg import LogRegModel
-from models.dnn import DNN, CrossValidationDNN
+from models.dnn import DNN, CrossValidationDNN, rmse
 from sklearn.feature_selection import VarianceThreshold, SelectKBest, chi2, SelectFromModel
 from sklearn.svm import LinearSVC
 
@@ -11,23 +11,23 @@ from sklearn.svm import LinearSVC
 
 transcripts = transcript.get_transcripts_in_path('./transcripts')
 p = Preprocessor('./transcripts', transcripts, 'train_split_Depression_AVEC2017.csv')
-x, y = p.get_all_transcript_features({
-    "liwc": True,
-    "liwc_indexes": list(range(80)),
-    "sentiment": True,
+x, y, titles = p.get_all_transcript_features({
+    # "liwc": True,
+    # "liwc_indexes": list(range(80)),
+    # "sentiment": True,
     "lda": True,
-    "antidepressants": True,
-    "absolutist": True
+    # "antidepressants": True,
+    # "absolutist": True
 })
 
 p_final = Preprocessor('./transcripts', transcripts, 'dev_split_Depression_AVEC2017.csv')
-x_final, y_final = p_final.get_all_transcript_features({
-    "liwc": True,
-    "liwc_indexes": list(range(80)),
-    "sentiment": True,
+x_final, y_final, titles = p_final.get_all_transcript_features({
+    # "liwc": True,
+    # "liwc_indexes": list(range(80)),
+    # "sentiment": True,
     "lda": True,
-    "antidepressants": True,
-    "absolutist": True
+    # "antidepressants": True,
+    # "absolutist": True
 })
 
 # Feature selection.
@@ -39,12 +39,6 @@ x_final, y_final = p_final.get_all_transcript_features({
 #x = sel.fit_transform(x, y)
 # print(new_x.shape)A
 
-# up sampling
-i_nd = np.where(y == 0)[0]
-i_d = np.where(y == 1)[0]
-i_d_upsampled = np.random.choice(i_d, size=len(i_nd), replace=True)
-y = np.concatenate((y[i_d_upsampled], y[i_nd]))
-x = np.concatenate((x[i_d_upsampled], x[i_nd]))
 
 seed = 5
 np.random.seed(seed)
@@ -57,20 +51,25 @@ train_y = y[train_index]
 test_x = x[test_index]
 test_y = y[test_index]
 
+# up sampling
+i_nd = np.where(train_y == 0)[0]
+i_d = np.where(train_y == 1)[0]
+i_d_upsampled = np.random.choice(i_d, size=len(i_nd), replace=True)
+train_y = np.concatenate((train_y[i_d_upsampled], train_y[i_nd]))
+train_x = np.concatenate((train_x[i_d_upsampled], train_x[i_nd]))
+
 # log_reg_model = LogRegModel(train_x, train_y, test_x, test_y)
 # log_reg_model.train()
 
-# dnn = DNN(train_x, train_y, test_x, test_y, 0.002)
-# model = dnn.train()
+dnn = DNN(train_x, train_y, test_x, test_y, 0.001)
+model = dnn.train()
 
-'''
-predictions = model.predict(x_final)
-corrects = []
-for i in range(len(predictions)):
-    corrects.append(y_final[i] == round(predictions[i][0]))
+print(model.metrics_names)
+print(model.test_on_batch(x_final, y_final))
 
-print(sum(corrects)/len(corrects))
-'''
+#rint("test accuracy:", sum(corrects)/len(corrects))
+#print("rmse: ", rmse(y_final, predictions))
 
-cv_dnn = CrossValidationDNN(train_x, train_y, test_x, test_y, 0.001, 3)
-cv_dnn.train_with_cross_validation()
+# cv_dnn = CrossValidationDNN(train_x, train_y, test_x, test_y, x_final, y_final, 0.001, 3)
+# cv_dnn.train_with_cross_validation()
+
